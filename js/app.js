e56453a95a671f144a0f2ce096eb3b4967d10d5e
Sal Cerera <33553528+salcerera@users.noreply.github.com>
2018-07-29 11:46:02 +0800

    initial commit

---
+++ js/app.js
@@ -0,0 +1,22 @@
+// Smooth Scroll
+$(document).ready(function() {
+    $('.smooth-scroll').click(function(e) {
+  
+      var linkHref = $(this).attr('href');
+      $('html, body').animate({
+        scrollTop: $(linkHref).offset().top
+      }, 1000);
+      e.preventDefault();
+  
+    });
+  
+});
+
+window.onscroll = function() {scrollFunction()};
+function scrollFunction() {
+    if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 500) {
+        document.getElementById("b2top").style.display = "block";
+    } else {
+        document.getElementById("b2top").style.display = "none";
+    }
+}
